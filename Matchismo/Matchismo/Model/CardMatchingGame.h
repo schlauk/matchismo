0bb6ee1dc65417d944034b1169d03e6e13394dd2
Karsten Schlautmann <github@karstens-email.de>
2013-11-03 14:45:15 +0100

    Lecture 3 code

---
+++ Matchismo/Matchismo/Model/CardMatchingGame.h
@@ -0,0 +1,24 @@
+//
+//  CardMatchingGame.h
+//  Matchismo
+//
+//  Created by Karsten Schlautmann on 03.11.13.
+//  Copyright (c) 2013 KarstensCode. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Deck.h"
+#import "Card.h"
+
+@interface CardMatchingGame : NSObject
+
+// designated initializer
+- (instancetype)initWithCardCount:(NSUInteger)count
+                        usingDeck:(Deck *)deck;
+
+- (void)chooseCardAtIndex:(NSUInteger)index;
+- (Card *)cardAtIndex:(NSUInteger)index;
+
+@property (nonatomic, readonly) NSInteger score;
+
+@end
